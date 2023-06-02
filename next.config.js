b39047d5e7368d67f5bf7029a898ca5778fa65d7
Alex MELHEM <alex.melhem@student-cs.fr>
2023-06-02 02:57:47 +0200

    Configure domains

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = nextConfig
 module.exports = {
   env: {
     imdbPhotoURL : "https://image.tmdb.org/t/p/original",
-    backURL : "http://10.144.57.12:8080",
+    backURL : "http://wassimovie.com/api",
     logoURL : "https://cdn-pop.viarezo.fr/static/wassimovie/logos/logo-white.png",
     blackLogoURL : "https://cdn-pop.viarezo.fr/static/wassimovie/logos/logo.png",
     smallLogoURL : "https://cdn-pop.viarezo.fr/static/wassimovie/logos/logo-small.png"
