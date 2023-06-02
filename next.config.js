97e510af1e1115909c6113147605b4760902ad07
Alex MELHEM <alex.melhem@student-cs.fr>
2023-06-02 05:53:52 +0200

    https for backURl

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = nextConfig
 module.exports = {
   env: {
     imdbPhotoURL : "https://image.tmdb.org/t/p/original",
-    backURL : "http://wassimovie.com/api",
+    backURL : "https://wassimovie.com/api",
     logoURL : "https://cdn-pop.viarezo.fr/static/wassimovie/logos/logo-white.png",
     blackLogoURL : "https://cdn-pop.viarezo.fr/static/wassimovie/logos/logo.png",
     smallLogoURL : "https://cdn-pop.viarezo.fr/static/wassimovie/logos/logo-small.png"
